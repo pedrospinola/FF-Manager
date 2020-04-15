55dbf8b77194a7357dda3569b22baa406f10ea88
Pedro Henrique Spínola de Assis <pedroassis@br-ufpe-03.local>
2020-04-15 12:16:49 -0300

    Mercado de compra e venda funcionando. Próximo passo: escalação

---
+++ FF-Manager.playground/Sources/TimeFF.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public class Time {
+    
+    var backgroundColor: String
+    var cash: Float
+    var nome: String
+    var logo: String
+    
+    init(backgroundColor: String, cash: Float, nome: String, logo: String) {
+        self.backgroundColor = backgroundColor
+        self.cash = cash
+        self.nome = nome
+        self.logo = logo
+    }
+    
+}
